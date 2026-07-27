de0e61e510a607f7d95ec11d400807fed6f800c2
kshitijdubeybtech2025-stack <kshitij.dubey.btech2025@sitpune.edu.in>
2026-07-27 11:20:39 +0530

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+        int[] numbers = {2, 30, 76, 58, 48};
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
